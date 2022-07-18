5651d37dbb6c9c839d0ef0f365d1e83661b64d96
LiaoShihua <shihua@iscas.ac.cn>
2022-07-18 18:01:56 +0800

    Add testcase for Packed Simd extension

---
+++ gas/testsuite/gas/riscv/rvp_csr.d
@@ -0,0 +1,14 @@
+#as: -march=rv32gc_zpn
+#source: rvp_csr.s
+#objdump: -dr
+
+.*:[ 	]+file format .*
+
+
+Disassembly of section .text:
+
+0+000 <rvp>:
+[ 	]+[0-9a-f]+:[ 	]+00905073[ 	]+csrwi[ 	]+vxsat,0
+[ 	]+[0-9a-f]+:[ 	]+0090f073[ 	]+csrci[ 	]+vxsat,1
+[ 	]+[0-9a-f]+:[ 	]+009022f3[ 	]+csrr[ 	]+t0,vxsat
+
